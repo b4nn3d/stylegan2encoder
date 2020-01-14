21cd0b0d03dc0a081ae21a106594725f91058eab
b4nn3d <miserabile@gmail.com>
2020-01-14 10:46:59 +0100

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -33,7 +33,7 @@ def generate_images(network_pkl, seeds, truncation_psi):
         z = rnd.randn(1, *Gs.input_shape[1:]) # [minibatch, component]
         tflib.set_vars({var: rnd.randn(*var.shape.as_list()) for var in noise_vars}) # [height, width]
         images = Gs.run(z, None, **Gs_kwargs) # [minibatch, height, width, channel]
-        PIL.Image.fromarray(images[0], 'RGB').save(dnnlib.make_run_dir_path('seed%04d.png' % seed))
+        PIL.Image.fromarray(images[0], 'RGB').save(dnnlib.make_run_dir_path('seed%04d.jpg' % seed))
 
 #----------------------------------------------------------------------------
 
